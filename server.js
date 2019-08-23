391eb489598d6b94ce7c0cae2303851f32ae7308
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-08-22 17:20:19 -0700

    added handlebars files and folders and added the necessary content to the server file

--- server.js
+++ server.js
@@ -1,3 +1,30 @@
-var express = require('express');
+// Dependencies
+// =============================================================
+var express = require("express");
 
+// Sets up the Express App
+// =============================================================
 var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Gets the routes from the applications
+var routes = require("./controllers/burger_controller");
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
+//makes sure the routes are used for the application
+app.use(routes);
