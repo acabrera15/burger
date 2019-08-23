a4ac9e416de2d3f51b806cb0e3b450db9896eb9a
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-08-22 17:01:08 -0700

    added 2 funtions to the burger model

--- models/burger.js
+++ models/burger.js
@@ -1,11 +1,19 @@
-var orm = require('../config/orm');
+var orm = require("../config/orm");
 
 var burgerModel = {
-    selectAllBugers: function() {
-        orm.selectAll("burgers", function(a) {
-            console.log(a);
-        });
-    }
-}
-
-burgerModel.selectAllBugers();
+  selectAllBugers: function() {
+    orm.selectAll("burgers", function(a) {
+      console.log(a);
+    });
+  },
+  insertBurger: function(name, devoured) {
+    orm.insertOne(
+      "burgers",
+      ["burger_name", "devoured"],
+      [name, devoured],
+      function(a) {
+        console.log(a);
+      }
+    );
+  }
+};
