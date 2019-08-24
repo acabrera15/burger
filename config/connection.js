9490b53b8715c260be16d788988ce2af1078adba
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-08-24 16:43:08 -0700

    added code to push mysql DB to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,21 @@
 //import mysql
-var mysql      = require('mysql');
+var mysql = require("mysql");
+var connection;
 
-//create a connection to mysql Db
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '@Ranger1',
-  database : 'burgers_db'
-});
- 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
+
 //starts connection to DB
 connection.connect(function(err) {
-    if (err) throw err;
-
+  if (err) throw err;
 });
 
 //export connection
